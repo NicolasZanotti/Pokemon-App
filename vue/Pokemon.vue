c7b623fe045300b0172be102fc092068a3ef92cf
Nicolas Zanotti <nicolaszanotti@me.com>
2021-11-20 14:41:45 +0100

    complete basic implementation

--- vue/Pokemon.vue
+++ vue/Pokemon.vue
@@ -10,10 +10,11 @@
 <template>
   <div class="pokemon">
     <h1>List of Pokémon</h1>
-    <ul></ul>
+    <ul v-if="names.length > 0">
+      <li v-for="name in names">{{ name }}</li>
+    </ul>
     <button @click="prev" v-bind:disabled="offset === 0">Previous</button>
-    <button @click="next" v-bind:disabled="offset === 30">Next</button>
-    <div>{{ offset }}</div>
+    <button @click="next" v-bind:disabled="offset >= 50">Next</button>
   </div>
 </template>
 
@@ -21,17 +22,32 @@
 export default {
   data() {
     return {
-      offset: 0
+      offset: 0,
+      names: [, , , , , , , , , ,],
     };
   },
   methods: {
     prev() {
       this.offset -= 10;
+      this.update();
     },
 
     next() {
       this.offset += 10;
+      this.update();
     },
+
+    async update() {
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${this.offset}`);
+        if (!response.ok) return;
+        const obj = await response.json();
+        this.names = obj.results.map(a => a.name);
+      } catch (error) {}
+    },
+  },
+  created() {
+    this.update();
   },
 };
 </script>

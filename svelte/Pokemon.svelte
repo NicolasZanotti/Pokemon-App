7e45d6d4f9f5c2731681f6e779b39fb2f073eaf2
Nicolas Zanotti <nicolaszanotti@me.com>
2021-11-21 11:37:17 +0100

    add base implementation

---
+++ svelte/Pokemon.svelte
@@ -0,0 +1,45 @@
+<script>
+  let offset = 0;
+
+  function next() {
+
+  }
+
+  function prev() {
+    
+  }
+</script>
+
+<div class="pokemon">
+  <h1>List of Pokémon</h1>
+  <ul>
+    <li>░░░░░░</li>
+    <li>░░░░░</li>
+    <li>░░░░░░░</li>
+    <li>░░░░</li>
+    <li>░░░░░░░░░</li>
+    <li>░░</li>
+    <li>░░░░░░░</li>
+    <li>░░░░░</li>
+    <li>░░░░</li>
+    <li>░░░</li>
+  </ul>
+  <div>{offset}</div>
+  <button disabled>Previous</button>&nbsp;<button>Next</button>
+</div>
+
+<style>
+  .pokemon {
+    font-family: sans-serif;
+  }
+
+  .pokemon ul {
+    position: relative;
+    padding-left: 1rem;
+  }
+
+  .pokemon li {
+    font-size: 1rem;
+    margin-bottom: 0.2rem;
+  }
+</style>
